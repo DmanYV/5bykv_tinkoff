eca3329027cb78f68e3e8fecdf57588ea2470c81
DmanYV <dima-xxx@mail.ru>
2024-05-04 00:34:08 +0300

    add 2 url

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://landofgames.ru/articles/guides/22117-otvety-na-igru-5-bukv-v-tinkoff-s-6-oktjabrja-po-20-dekabrja.html'
+url1 = 'https://landofgames.ru/articles/guides/22117-otvety-na-igru-5-bukv-v-tinkoff-s-6-oktjabrja-po-20-dekabrja.html'
+url2 = 'https://between-us-girls.ru/5-bukv-tinkoff-segodnya-otvet/'
 
 
-def get_request():
-    response = requests.get(url=url)
+def get_request1():
+    response = requests.get(url=url2)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    data = soup.find('td').text
+    request = soup.find('td').find_next('td').text
+    print(f'Ресурс 1:\nНа {data}\nЗагаданное слово: {request}\n')
+
+
+def get_request2():
+    response = requests.get(url=url1)
     soup = BeautifulSoup(response.text, 'html.parser')
     data = soup.find('h3').text
     request = soup.find('mark', class_='m3').text
-    print(f'На {data}\nЗагаданное слово: {request}')
+    print(f'Ресурс 2:\nНа {data}\nЗагаданное слово: {request}')
 
 
 if __name__ == '__main__':
-    get_request()
+    get_request1()
+    get_request2()
