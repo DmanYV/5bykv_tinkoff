4f0ffd3654dcc60674e3f19f0e8bc85870044814
DmanYV <dima-xxx@mail.ru>
2024-05-03 22:47:43 +0300

    rename def get_otvet - get_request

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 url = 'https://landofgames.ru/articles/guides/22117-otvety-na-igru-5-bukv-v-tinkoff-s-6-oktjabrja-po-20-dekabrja.html'
 
 
-def get_otvet():
+def get_request():
     response = requests.get(url=url)
     soup = BeautifulSoup(response.text, 'html.parser')
     data = soup.find('h3').text
@@ -13,4 +13,4 @@ def get_otvet():
 
 
 if __name__ == '__main__':
-    get_otvet()
+    get_request()
